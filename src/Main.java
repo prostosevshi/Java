5241959e8d070d859028084e7bbcbae496ec1c39
prostosevshi <prostosevshi@gmail.com>
2023-05-20 21:36:54 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //task_1();
+        //task_2();
+        //task_3();
+        //task_4();
+    }
+}
