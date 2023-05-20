5a6fe2d7bd9f1c487330b88b7646220ba61bf3ee
prostosevshi <prostosevshi@gmail.com>
2023-05-20 21:56:09 +0400

    changing structure

---
+++ src/package_1/lesson_1/lesson_1.java
@@ -0,0 +1,32 @@
+package package_1.lesson_1;
+
+import java.util.Scanner;
+
+public class lesson_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int randomNumber = (int)(Math.random() * 21);
+
+        System.out.println("Игра - Угадайте число");
+
+        while (true)
+        {
+            System.out.print("Введите число: ");
+            int userInput = scanner.nextInt();
+
+            if (userInput == randomNumber)
+            {
+                System.out.println("Вы угадали!!!");
+                break;
+            }
+            else if (userInput > randomNumber)
+            {
+                System.out.println("Ваше число больше!");
+            }
+            else
+            {
+                System.out.println("Ваше число меньше!");
+            }
+        }
+    }
+}
